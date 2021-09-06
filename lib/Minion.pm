92d0faff9ccbd3c77ab8080a89a9762f00ac1c75
Gauthier Voron <gauthier.voron@epfl.ch>
2021-09-06 10:00:49 +0200

    chore: all: initial commit
    Add an empty perl library structure.

---
+++ lib/Minion.pm
@@ -0,0 +1,60 @@
+package Minion;
+
+use 5.034000;
+use strict;
+use warnings;
+
+our @ISA = qw();
+
+our $VERSION = '0.01';
+
+
+# Preloaded methods go here.
+
+1;
+__END__
+# Below is stub documentation for your module. You'd better edit it!
+
+=head1 NAME
+
+Minion - Perl extension for blah blah blah
+
+=head1 SYNOPSIS
+
+  use Minion;
+  blah blah blah
+
+=head1 DESCRIPTION
+
+Stub documentation for Minion, created by h2xs. It looks like the
+author of the extension was negligent enough to leave the stub
+unedited.
+
+Blah blah blah.
+
+
+=head1 SEE ALSO
+
+Mention other useful documentation such as the documentation of
+related modules or operating system documentation (such as man pages
+in UNIX), or any relevant external documentation such as RFCs or
+standards.
+
+If you have a mailing list set up for your module, mention it here.
+
+If you have a web site set up for your module, mention it here.
+
+=head1 AUTHOR
+
+A. U. Thor, E<lt>gauthier@nonetE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2021 by A. U. Thor
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.34.0 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
