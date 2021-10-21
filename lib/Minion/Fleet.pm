f789ffa319b3ff89aa13d4cb870baf1fa83840bd
Gauthier Voron <gauthier.voron@epfl.ch>
2021-10-21 23:38:30 +0200

    feat:fleet: add a recv method to the fleet
    Add a `Fleet->recv()` method which works the same as for
    `Worker->recv()` except that the `TARGET` option is replaced by a
    `TARGETS` option which also accepts an ARRAY as value. In this case,
    each element of the ARRAY is transmitted to the members when calling
    their `Worker->recv()` methods.

--- lib/Minion/Fleet.pm
+++ lib/Minion/Fleet.pm
@@ -142,6 +142,49 @@ sub send
     return $ret;
 }
 
+sub recv
+{
+    my ($self, $sources, %opts) = @_;
+    my (@members, $member, $value, $i);
+    my (@moptss, $mopts, $ret, $proc);
+
+    confess() if (!defined($sources));
+    confess() if (ref($sources) ne 'ARRAY');
+    confess() if (grep { ref($_) ne '' } @$sources);
+
+    @members = $self->members();
+
+    foreach $member (@members) {
+	push(@moptss, {});
+    }
+
+    if (defined($value = $opts{TARGETS})) {
+	if (ref($value) eq 'ARRAY') {
+	    confess() if (scalar(@$value) != scalar(@members));
+	    for ($i = 0; $i < scalar(@$value); $i++) {
+		$moptss[$i]->{TARGET} = $value->[$i];
+	    }
+	} else {
+	    for ($i = 0; $i < scalar(@members); $i++) {
+		$moptss[$i]->{TARGET} = $value;
+	    }
+	}
+	delete($opts{TARGETS});
+    }
+
+    confess(join(' ', keys(%opts))) if (%opts);
+
+    $ret = Minion::System::Pgroup->new([]);
+
+    foreach $member (@members) {
+	$mopts = shift(@moptss);
+	$proc = $member->recv($sources, %$mopts);
+	$ret->add($proc);
+    }
+
+    return $ret;
+}
+
 
 1;
 __END__
