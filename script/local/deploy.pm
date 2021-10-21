0f1891c3fcea2d3248c866ccd4f88d0156513bf1
Gauthier Voron <gauthier.voron@epfl.ch>
2021-10-22 00:05:16 +0200

    feat:run: add a script for full deployment
    Add a script to deploy every nodes which have been configured to behave
    at either Algorand or Diablo.

---
+++ script/local/deploy.pm
@@ -0,0 +1,35 @@
+package deploy;
+
+use strict;
+use warnings;
+
+
+my $FLEET = $_;
+my %PARAMS = @_;
+my $RUNNER = $PARAMS{RUNNER};
+
+
+# Remove the previous deployment directory to start from a clean one.
+#
+
+$FLEET->execute(['rm', '-rf', 'deploy'], STDERRS => '/dev/null')->waitall();
+
+# Deploy the blockchains that have been enabled by 'behave-*' scripts.
+# Do nothing if not enabled.
+#
+
+if ($RUNNER->run($FLEET, [ 'deploy-algorand' ])->wait() != 0) {
+    die ("failed to deploy algorand");
+}
+
+# Deploy diablo at the very end as it might need some configuration generated
+# by the deployment of other blockchains.
+#
+
+if ($RUNNER->run($FLEET, [ 'deploy-diablo' ])->wait() != 0) {
+    die ("failed to deploy diablo");
+}
+
+
+1;
+__END__
