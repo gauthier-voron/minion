4dfddce3cbb1107d59f6fa93331731a30cf6ede4
Gauthier Voron <gauthier.voron@epfl.ch>
2021-10-21 23:46:52 +0200

    test:worker: add a mock worker
    Add a `MockWorker` for unit tests. This worker does nothing except
    logging the calls to its methods.

---
+++ t/Minion/MockWorker.pm
@@ -0,0 +1,72 @@
+package Minion::MockWorker;
+
+use parent qw(Minion::Worker);
+use strict;
+use warnings;
+
+use Carp qw(confess);
+
+use Minion::Worker;
+
+
+sub _init
+{
+    my ($self, %opts) = @_;
+
+    if (!defined($self->SUPER::_init(%opts))) {
+	return undef;
+    }
+
+    $self->{__PACKAGE__()}->{_mocklog} = [];
+
+    return $self;
+}
+
+
+sub _log_mock
+{
+    my ($self, $entry) = @_;
+
+    push(@{$self->{__PACKAGE__()}->{_mocklog}}, $entry);
+}
+
+sub mock_log
+{
+    my ($self, @err) = @_;
+
+    confess() if (@err);
+
+    return $self->{__PACKAGE__()}->{_mocklog};
+}
+
+
+sub execute
+{
+    my ($self, $cmd, %opts) = @_;
+
+    $self->_log_mock(['execute', [ @$cmd ], { %opts }]);
+
+    return undef;
+}
+
+sub send
+{
+    my ($self, $sources, %opts) = @_;
+
+    $self->_log_mock(['send', [ @$sources ], { %opts }]);
+
+    return undef;
+}
+
+sub recv
+{
+    my ($self, $sources, %opts) = @_;
+
+    $self->_log_mock(['recv', [ @$sources ], { %opts }]);
+
+    return undef;
+}
+
+
+1;
+__END__
