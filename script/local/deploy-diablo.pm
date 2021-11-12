e97205dcf1674acb621da50ade736e1f0fa4d558
Gauthier Voron <gauthier.voron@epfl.ch>
2021-11-12 12:17:30 +0100

    feat:quorum: enable diablo to plug to quorum ibft
    Change the diablo deployment script to detect that there is a deployed
    Quorum IBFT chain and plug to it.

--- script/local/deploy-diablo.pm
+++ script/local/deploy-diablo.pm
@@ -34,6 +34,7 @@ my $KEYS_LOC = $DEPLOY . '/keys.json';
 
 
 my $ALGORAND_CHAINCONFIG_PATH = $SHARED . '/algorand-chain.yml';
+my $QUORUMIBFT_CHAIN_PATH = $SHARED . '/quorum-ibft/chain.yaml';
 
 
 # Extract from the given $path the Quorum nodes.
@@ -147,6 +148,10 @@ sub deploy_diablo_algorand
     return deploy_diablo_chain(@_, $ALGORAND_CHAINCONFIG_PATH);
 }
 
+sub deploy_diablo_quorum_ibft
+{
+    return deploy_diablo_chain(@_, $QUORUMIBFT_CHAIN_PATH);
+}
 
 
 
@@ -246,6 +251,10 @@ sub deploy_diablo
 
     if (-f $ALGORAND_CHAINCONFIG_PATH) {
 	return deploy_diablo_algorand($nodes, $primary, \@secondaries);
+    }
+
+    if (-f $QUORUMIBFT_CHAIN_PATH) {
+	return deploy_diablo_quorum_ibft($nodes, $primary, \@secondaries);
     }
 
 
