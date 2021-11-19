d1b067b03680b7db8d6a735140d21d7685a53de8
Gauthier Voron <gauthier.voron@epfl.ch>
2021-11-19 10:06:43 +0100

    feat:aws: add resize function to instances
    Add a `Minion::Aws::Instance->resize()` function to resize the first
    volume attached to the instance to the specified size in GB. The
    function waits for the modification to be effective.

--- lib/Minion/Aws/Instance.pm
+++ lib/Minion/Aws/Instance.pm
@@ -8,6 +8,7 @@ use Carp qw(confess);
 
 use Minion::Aws::Cli;
 use Minion::Ssh;
+use Minion::System::ProcessFuture;
 
 
 sub __check_ip4
@@ -45,6 +46,18 @@ sub _init
     confess() if (!__check_ip4($private_ip));
     confess() if ($fleet_id !~ /^sfr(?:-[0-9a-f]+)+$/);
 
+    if (defined($value = $opts{ERR})) {
+	$sopts{ERR} = $value;
+	$self->{__PACKAGE__()}->{_err} = $value;
+	delete($opts{ERR});
+    }
+
+    if (defined($value = $opts{LOG})) {
+	$sopts{LOG} = $value;
+	$self->{__PACKAGE__()}->{_log} = $value;
+	delete($opts{LOG});
+    }
+
     if (defined($value = $opts{REGION})) {
 	$self->{__PACKAGE__()}->{_region} = $value;
 	delete($opts{REGION});
@@ -53,11 +66,9 @@ sub _init
 	    Minion::Aws::Cli->get_region()->get();
     }
 
-    foreach $opt (qw(ERR LOG USER)) {
-	if (defined($value = $opts{$opt})) {
-	    $sopts{$opt} = $value;
-	    delete($opts{$opt});
-	}
+    if (defined($value = $opts{USER})) {
+	$sopts{USER} = $value;
+	delete($opts{USER});
     }
 
     confess(join(' ', keys(%opts))) if (%opts);
@@ -75,6 +86,65 @@ sub _init
 	    'aws:private-ip' => sub { return $self->private_ip() },
 	    'aws:region'     => sub { return $self->region() }
 	}, %sopts);
+}
+
+
+sub resize
+{
+    my ($self, $size, @err) = @_;
+    my (%fopts, %copts, $value, $log);
+
+    confess() if (@err);
+    confess() if (!defined($size));
+    confess() if (ref($size) ne '');
+    confess() if ($size !~ /^\d+$/);
+
+    if (defined($value = $self->{__PACKAGE__()}->{_err})) {
+	$fopts{STDERR} = $value;
+    }
+
+    if (defined($value = $self->{__PACKAGE__()}->{_log})) {
+	push(@{$fopts{IO}}, [ \$log, '>', $value ]);
+    }
+
+    return Minion::System::ProcessFuture->new(sub {
+	my ($volume, $cli, $mstate);
+
+	if (defined($log)) {
+	    $copts{LOG} = $log;
+	}
+
+	$volume = Minion::Aws::Cli->describe_volumes(
+	    FILTERS => { 'attachment.instance-id' => $self->id() },
+	    QUERY => 'Volumes[0].Attachments[0].VolumeId',
+	    %copts
+	    )->get();
+
+	if (!defined($volume)) {
+	    exit (1);
+	}
+
+	$cli = Minion::Aws::Cli->modify_volume($volume, SIZE => $size, %copts);
+	if ($cli->wait() != 0) {
+	    exit ($cli->exitstatus());
+	}
+
+	$mstate = 'modifying';
+
+	while ($mstate eq 'modifying') {
+	    $mstate = Minion::Aws::Cli->describe_volumes_modifications(
+		IDS => [ $volume ],
+		QUERY => 'VolumesModifications[0].ModificationState',
+		%copts
+		)->get();
+
+	    if (!defined($mstate)) {
+		exit (1);
+	    }
+
+	    sleep(5);
+	}
+    }, %fopts);
 }
 
 
