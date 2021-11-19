84063ea4ac2801cfae16369213efa090a4879d6f
Gauthier Voron <gauthier.voron@epfl.ch>
2021-11-19 11:17:38 +0100

    test:diablo: add counter smart contract
    Add a counter smart contract in Teal and Solidity as well as a workload
    YAML file to test them along with the static minion test tool.

---
+++ counter.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.0;
+
+contract Counter {
+    int private count = 0;
+
+    function Add() public {
+        count += 1;
+    }
+
+    function Sub() public {
+        if (count > 0) {
+            count -= 1;
+	}
+    }
+}
